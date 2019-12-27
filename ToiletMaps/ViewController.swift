48dcf16f860fd55bd2ee7aab79fd54af21508850
Pieter Paelinck <pieter.paelinck.y3320@student.hogent.be>
2019-12-27 11:26:58 +0100

    Initial Commit

---
+++ ToiletMaps/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToiletMaps
+//
+//  Created by Pieter Paelinck on 27/12/2019.
+//  Copyright © 2019 Pieter Paelinck. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
